4e4001c781f0f21efda5543725da0cde71a32692
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-11-12 21:19:22 -0800

    revamp settings app

--- src/components/apps/settings.tsx
+++ src/components/apps/settings.tsx
@@ -1,12 +1,34 @@
-import Window from "@/components/window";
-import styles  from "@/styles/settings.module.css";
-import {useState} from "react";
+import styles from "@/styles/settings.module.css";
+import { useState } from "react";
+import { useDraggable } from "@dnd-kit/core";
+import React from "react";
+import { useStateContext } from "../stateProvider";
 
 export default function Settings() {
-  const [selected, setSelected] = useState("Background");
+  const { state, setState } = useStateContext();
+  const [selected, setSelected] = useState();
+
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: "settings",
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
+  const closeApp = () => {
+    setState({ ...state, settings: -1 });
+  };
 
   return (
-    <Window title="Settings" app="settings">
+    <div
+      className={styles.window}
+      style={style}
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+    >
       <div className={styles.container}>
         <div className={styles.sections}>
           <div className={styles.section}>Background</div>
@@ -17,10 +39,8 @@ export default function Settings() {
           <div className={styles.section}></div>
           <div className={styles.section}></div>
         </div>
-        <div className={styles.test}>
-
-        </div>
+        <div className={styles.test}></div>
       </div>
-    </Window>
-  )
+    </div>
+  );
 }
